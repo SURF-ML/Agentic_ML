a4c346439e8b24fac11570bdf3d3745050c26935
Fjf <fjf@hotmail.nl>
2025-06-26 14:47:54 +0200

    ix

--- agentic_ml/orchestrator/agent_definitions.py
+++ agentic_ml/orchestrator/agent_definitions.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from agent.tools import (create_directory, write_file, append_to_file, read_file_content,
+from agentic_ml.agent.tools import (create_directory, write_file, append_to_file, read_file_content,
     list_directory_contents, delete_file_or_directory, replace_text_in_file,
     execute_python_script, execute_shell_command, install_python_package,
     browse_webpage, search_arxiv, search_github_repositories,
